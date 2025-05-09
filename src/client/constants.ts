03a994fb61b761403e44ad9b740008e6b549d0f2
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-09 17:43:22 +0200

    release fix

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -4,4 +4,4 @@ export const API_BASE_URL = "http://ec2-13-244-112-99.af-south-1.compute.amazona
 export const applicationUri = "http://ec2-13-244-112-99.af-south-1.compute.amazonaws.com";
 
 
-export const googleAuthURI = `https://accounts.google.com/o/oauth2/auth?client_id=1090642996208-ohdh4ge7agbi5egf22j3qapf2q3ko16a.apps.googleusercontent.com&redirect_uri=${API_BASE_URL}.html&response_type=code&scope=openid%20phone%20email%20profile`;
+export const googleAuthURI = `https://accounts.google.com/o/oauth2/auth?client_id=1090642996208-ohdh4ge7agbi5egf22j3qapf2q3ko16a.apps.googleusercontent.com&redirect_uri=${API_BASE_URL}/login.html&response_type=code&scope=openid%20phone%20email%20profile`;
