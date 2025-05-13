2af47a269fb06186c37d204eb3204e4efdb93136
VuyoSibiya-BBD <vuyo.sibiya@bbd.co.za>
2025-05-13 19:13:21 +0200

    removed question-audit

--- src/server/repositories/fill-blank-questions-audit-repository.ts
+++
@@ -1,9 +0,0 @@
-import { BaseRepository } from "../lib/base-repository";
-import { FillBlankQuestionsAudit } from "../lib/types";
-
-export class FillBlankQuestionsAuditRepository extends BaseRepository<FillBlankQuestionsAudit> {
-    constructor() {
-        super("fill_blank_questions_audit", "fill_blank_questions_audit_id");
-    }
-}
-
