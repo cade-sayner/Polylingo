b6921b811d67211e70bec1336ec8122c911fddc6
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-13 11:44:19 +0200

    small fix

--- public/js/components/language-selection.js
+++ public/js/components/language-selection.js
@@ -1,8 +1,6 @@
 export class LanguageSelection {
     render() {
         var _a, _b;
-        console.log("The actual thing is");
-        console.log("pppd" + document.querySelector("#language-select"));
         return (_b = (_a = document.querySelector("#language-select")) === null || _a === void 0 ? void 0 : _a.innerHTML) !== null && _b !== void 0 ? _b : "";
     }
 }
