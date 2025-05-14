0f68b534a2cf90c7492ef3516b133ea454166ae5
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-14 20:31:15 +0200

    RELEASE CHANGES

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,8 +1,8 @@
 export const URI = "test";
-export const googleAuthURI = "https://accounts.google.com/o/oauth2/auth?client_id=1090642996208-ohdh4ge7agbi5egf22j3qapf2q3ko16a.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login.html&response_type=code&scope=openid%20phone%20email%20profile";
-export const API_BASE_URL = "http://localhost:3000";
-export const applicationUri = "http://localhost:3000";
 export const languageOptions = ["Afrikaans", "German", "Italian", "Spanish", "French"];
 export const imageSrcs = ["springbok-speaker.png", "lion-character.png"];
 export const seaSponge: string = "rgb(215, 255, 184)";
 export const colorCrab: string = "rgb(255, 120, 120)";
+export const API_BASE_URL = "http://ec2-13-244-112-99.af-south-1.compute.amazonaws.com";
+export const applicationUri = "http://ec2-13-244-112-99.af-south-1.compute.amazonaws.com";
+export const googleAuthURI = `https://accounts.google.com/o/oauth2/auth?client_id=1090642996208-ohdh4ge7agbi5egf22j3qapf2q3ko16a.apps.googleusercontent.com&redirect_uri=${API_BASE_URL}/login.html&response_type=code&scope=openid%20phone%20email%20profile`;
--- src/server/lib/db.ts
+++ src/server/lib/db.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME || "postgres",
   password: process.env.DB_PASSWORD || "kakkakkak",
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  // ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function connectAndQuery(queryString: string, values: any[]) {
