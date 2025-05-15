2a5920971d38a55f0f05366fac1ff7e7c1023a1a
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-15 22:47:16 +0200

    release v3 changes

--- src/server/lib/db.ts
+++ src/server/lib/db.ts
@@ -1,13 +1,14 @@
 import camelcaseKeys from "camelcase-keys";
 import { Pool } from "pg";
+import 'dotenv/config'
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "postgres",
-  password: process.env.DB_PASSWORD || "kakkakkak",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  // ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function connectAndQuery(queryString: string, values: any[]) {
